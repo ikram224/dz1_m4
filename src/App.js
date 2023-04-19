51f6a72ba3f06b34e56d3eee221bf3b6d9d6a717
vundizow.ikram@yandex.ru <vundizow.ikram@yandex.ru>
2023-04-19 17:41:51 +0300

    qwerty

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {Remarkable} from "remarkable";
+
+class Plice extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {value : "привет мир!"}
+    }
+    handleChange(e) {
+        this.setState({value: e.target.value})
+    }
+    getRawMarkup() {
+        const md = new Remarkable()
+        return {__html: md.render(this.state.value)}
+    }
+    render() {
+        return (
+            <div className="Plice">
+                <h3>входные данные</h3>
+                <label htmlFor="markdown-content">
+                    ведите что то в форме Plice
+                </label>
+                <textarea id="markdown-content"
+                          onChange={this.handleChange}
+                          defaultValue={this.state.value}/>
+                <h3>вывод</h3>
+                <div className="content"
+                     dangerouslySetInnerHTML={this.getRawMarkup()}/>
+            </div>
+        )
+    }
+}
+
+
+
+export default Plice;
